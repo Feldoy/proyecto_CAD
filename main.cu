7a8d72b9cb0671d4b0131a868190df3bbfa59960
felipe.quintanilla <felipe.quintanilla@sansano.usm.cl>
2021-12-14 02:54:41 -0300

    Problemas con función CPU corregidos

--- main.cu
+++ main.cu
@@ -46,19 +46,22 @@ void ReadSoA(int** A, int** B, int *la, int *lb, const char *filename) {
 	*B = Btemp;
 }
 
-void Write(int* intersecciones, int N, const char *filename) {
+void Write(int* intersecciones, int la, int lb, const char *filename) {
 	FILE *fp;
 	fp = fopen(filename, "w");
-	for (int i = 0; i < N; i++){
+
+	for (int i = 0; i < (la*lb*2); i++){
 		if (i%2 == 0){
+			if ((i != 0) && (intersecciones[i] == 0) && (intersecciones[i+1] == 0)){
+				break;
+			}
 			fprintf(fp, "%d %d\n", intersecciones[i], intersecciones[i + 1]);
 		}
 	}
 	fclose(fp);
 }
 
-
-bool seInterseca(int aStart, int aEnd, int bStart, int bEnd){
+bool seIntersecta(int aStart, int aEnd, int bStart, int bEnd){
 
 	if ((aEnd < bStart) || (bEnd < aStart)){
 		return false;
@@ -75,8 +78,8 @@ for i en el largo de A
     Si A[i] se intersecta con B[j]
         Guardar (i,j)
 */
-void interseccionConjuntos(int* A, int *B, int *intersecciones,
-							int la, int lb){		
+void interseccionConjuntos(int* A, int *B,int *intersecciones, 
+						int la, int lb){		
 	
 	int aStart, aEnd, bStart, bEnd;
 	int posicion = 0;
@@ -89,8 +92,9 @@ void interseccionConjuntos(int* A, int *B, int *intersecciones,
 			bStart = B[2*j];
 			bEnd = B[2*j + 1];
 
-			if (seInterseca(aStart, aEnd, bStart, bEnd)){
+			if (seIntersecta(aStart, aEnd, bStart, bEnd)){
 				//Guardo el número del intervalo (partiendo de 0)
+				printf("%d ", posicion);
 				intersecciones[posicion] = i;
 				intersecciones[posicion + 1] = j;
 				posicion += 2;
@@ -113,10 +117,10 @@ int main(int argc, char **argv){
 
 	Read(&A, &B, &la, &lb, filename); 
 
-	for (int i = 0; i < lb*2; i++){
-		std::cout << B[i] << std::endl;
-	}
-	
+	// for (int i = 0; i < la*2; i++){
+	// 	std::cout << A[i] << std::endl;
+	// }
+
 	// Parte CPU
 
 	intersecciones = new int[la*lb*2];
@@ -128,10 +132,10 @@ int main(int argc, char **argv){
 	double ms = 1000.0 * (double)(t2 -t1) / CLOCKS_PER_SEC;
 
 	std::cout << "Tiempo algoritmo en CPU = " << ms << "[ms]" << std::endl;
-	
-	int N = sizeof(intersecciones);
 
-	Write(intersecciones, N, outputFilename);
+	Write(intersecciones, la, lb, outputFilename);
 
 	delete[] intersecciones;
+
+	return 0;
 }
